0fee7e9edd125cde9dcef56df1596105c22250de
Domício Medeiros <domicioam@gmail.com>
2021-03-11 18:26:02 +0000

    Add project files.

---
+++ WpfAkka/AsyncDemoActor.cs
@@ -0,0 +1,60 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfAkka
+{
+    public class AsyncDemoActor : ReceiveActor
+    {
+        #region Messages
+
+        public class WithGetAwaiter { }
+        public class WithPipeTo { }
+        public class HelloWorld { }
+
+        #endregion
+
+        int counter = 1;
+
+        public AsyncDemoActor(TextBlock textBlock)
+        {
+            Receive<WithGetAwaiter>(msg =>
+            {
+                counter = 1;
+
+                Context.System.Scheduler.ScheduleTellRepeatedly(
+                    TimeSpan.FromMilliseconds(250),
+                    TimeSpan.FromMilliseconds(250),
+                    Self,
+                    new HelloWorld(),
+                    Self);
+
+                Task.Delay(TimeSpan.FromSeconds(5)).GetAwaiter().GetResult();
+            });
+
+            Receive<WithPipeTo>(msg =>
+            {
+                counter = 1;
+
+                Context.System.Scheduler.ScheduleTellRepeatedly(
+                    TimeSpan.FromMilliseconds(250),
+                    TimeSpan.FromMilliseconds(250),
+                    Self,
+                    new HelloWorld(),
+                    Self);
+
+                Task.Delay(TimeSpan.FromSeconds(5)).PipeTo(Self);
+            });
+
+            Receive<HelloWorld>(msg =>
+            {
+                textBlock.Text = $"Hello World {counter}";
+                counter++;
+            });
+        }
+    }
+}
---
+++ WpfAkka/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Akka.Actor;
+using Akka.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAkka
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IActorRef actor;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // make the actor run in the UI thread to demonstrate the issue
+            var config = ConfigurationFactory.ParseString(
+                @"
+                    akka {
+                      actor {
+                        deployment {
+                          /asyncDemoActor {
+                            dispatcher = akka.actor.synchronized-dispatcher
+                          }
+                        }
+                      }
+                    }
+                ");
+            
+            var actorSystem = ActorSystem.Create("ActorSystem", config);
+            actor = actorSystem.ActorOf(Props.Create(() => new AsyncDemoActor(textBlock)), "asyncDemoActor");
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            actor.Tell(new AsyncDemoActor.WithGetAwaiter());
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            actor.Tell(new AsyncDemoActor.WithPipeTo());
+        }
+    }
+}
